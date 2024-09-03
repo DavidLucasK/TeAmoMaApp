6c8b9cf98dda7820cf5b38922022286d4c96b598
DavidLucasK <davidlucasfr70@gmail.com>
2024-09-03 08:10:12 -0300

    ADding thing to posts

--- src/screens/Posts.tsx
+++ src/screens/Posts.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
 import PostsStyles from '../styles/PostsStyles'; // Importando os estilos do Home
 import { LinearGradient } from 'expo-linear-gradient'; // Importando LinearGradient
@@ -6,9 +6,24 @@ import { useNavigation } from '@react-navigation/native'; // Importando useNavig
 import { PostsNavigationProp } from '../navigation'; // Importando o tipo de navegação
 import Header from '../components/Header';
 
-
 const Posts: React.FC = () => {
     const navigation = useNavigation<PostsNavigationProp>(); // Usando o tipo de navegação para HomeScreen
+    const [likedPosts, setLikedPosts] = useState<number[]>([]);
+
+    const toggleLike = (postId: number) => {
+        if (likedPosts.includes(postId)) {
+            setLikedPosts(likedPosts.filter((id) => id !== postId));
+        } else {
+            setLikedPosts([...likedPosts, postId]);
+        }
+    };
+
+    const posts = [
+        { id: 1, user: 'Mazinha02', tempo: '12 horas', imageSource: require('./assets/teamo1.png'), text: 'Oi amor te amo muito, tá?' },
+        { id: 2, user: 'Mazinha02', tempo: '12 horas', imageSource: require('./assets/teamo2.png'), text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cum odit unde veritatis omnis optio fugit a. Corporis quia illum aspernatur omnis, distinctio assumenda sunt praesentium commodi. Corporis, eveniet quaerat!' },
+        { id: 3, user: 'Mazinha02', tempo: '12 horas', imageSource: require('./assets/sobremesa.png'), text: 'Texto post 2' },
+        { id: 4, user: 'Mazinha02', tempo: '12 horas', imageSource: require('./assets/cinema.png'), text: 'Oi amor te amo muito, tá?' },
+    ];
 
     return (
         <View style={PostsStyles.container}>
@@ -16,7 +31,7 @@ const Posts: React.FC = () => {
                 leftIcon={require('./assets/store.png')}
                 middleIcon={require('./assets/game.png')}
                 rightIcon={require('./assets/profile-user.png')}
-                onLeftIconPress={() => navigation.navigate('Store')} // Passando a função
+                onLeftIconPress={() => navigation.navigate('Store')}
                 onMiddleIconPress={() => navigation.navigate('EarnPoints')}
                 onRightIconPress={() => navigation.navigate('Profile')}
                 isStoreScreen={false}
@@ -24,17 +39,38 @@ const Posts: React.FC = () => {
             <ScrollView contentContainerStyle={PostsStyles.scrollContainer}>
                 <LinearGradient
                     colors={['#e41d69', '#fe8277']}
-                    start={{ x: 0, y: 0 }} // Início do gradiente (canto superior esquerdo)
+                    start={{ x: 0, y: 0 }}
                     end={{ x: 1, y: 0 }}
-                    style={PostsStyles.main} // Usando o estilo do cabeçalho
+                    style={PostsStyles.main}
                 >
                     <View style={PostsStyles.main}>
                         <ScrollView>
                             <View style={PostsStyles.postsContainer}>
-                                <View style={PostsStyles.post}>
-                                    <Image style={PostsStyles.imagePost} source={require('./assets/cinema.png')} />
-                                    <Text style={PostsStyles.textBottom}>Oi legal maneiro top</Text>
-                                </View>
+                                <TouchableOpacity style={PostsStyles.plusBtn}>
+                                    <Image style={PostsStyles.plus} source={require('./assets/plus.png')} />
+                                </TouchableOpacity>
+                                {posts.map((post) => (
+                                    <View key={post.id} style={PostsStyles.post}>
+                                        <Text style={PostsStyles.user}>{post.user}</Text>
+                                        <Text style={PostsStyles.tempo}>Há {post.tempo}</Text>
+                                        <View style={PostsStyles.imageContainer}>
+                                            <Image style={PostsStyles.imagePost} source={post.imageSource} />
+                                        </View>
+                                        <Text style={PostsStyles.textBottom}>{post.text}</Text>
+                                        <View style={PostsStyles.iconsContainer}>
+                                            <TouchableOpacity onPress={() => toggleLike(post.id)}>
+                                                <Image
+                                                    style={PostsStyles.iconsContainer}
+                                                    source={
+                                                        likedPosts.includes(post.id)
+                                                            ? require('./assets/heartFilled.png')
+                                                            : require('./assets/heartNoFill.png')
+                                                    }
+                                                />
+                                            </TouchableOpacity>
+                                        </View>
+                                    </View>
+                                ))}
                             </View>
                         </ScrollView>
                     </View>
--- src/styles/PostsStyles.tsx
+++ src/styles/PostsStyles.tsx
@@ -7,7 +7,6 @@ const PostsStyles = StyleSheet.create({
     main: {
         width: '100%',
         height: '100%',
-        paddingTop: 20, // 7.5rem em pixels
         textAlign: 'center',
         paddingHorizontal: 10,
     },
@@ -17,19 +16,82 @@ const PostsStyles = StyleSheet.create({
     },
     postsContainer: {
         width: '100%',
+        display: 'flex',
         paddingHorizontal: '5%',
+        marginVertical: '10%',
         borderRadius: 30,
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
         backgroundColor: '#FFF',
     },
+    plusBtn: {
+        display: 'flex',
+        height: 40,
+        marginVertical: 15,
+    },
+    plus: {
+        width: '100%',
+        height: '100%',
+        alignSelf: 'center',
+        marginBottom: 0,
+        paddingBottom: 0,
+        resizeMode: 'contain',
+    },
+    posts: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
     post: {
-
+        marginBottom: 64,
+        alignSelf: 'center',
+    },
+    user: {
+        fontFamily: 'Poppins_700Bold',
+        fontSize: 20,
+        color: '#ff0055',
+        marginBottom: -10,
+    },
+    imageContainer: {
+        minWidth: 285,
+        minHeight: 405, // Ajuste conforme necessário
+        maxWidth: 285,
+        maxHeight: 255, // Ajuste conforme necessário
+        marginBottom: 16,
+        backgroundColor: '#000',
+        overflow: 'hidden', // Garante que o conteúdo não ultrapasse o
+        borderWidth: 0,
+        borderColor: '#000', // Cor da borda
+        borderRadius: 10,
+    },
+    iconsContainer:{
+        display: 'flex',
+        width: 40,
+        height: 40,
+        marginVertical: 10,
+    },
+    heart: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain',
     },
     imagePost: {
-        width: '100%', // 25rem em pixels
-        objectFit: 'contain',
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain', // Usa o
     },
     textBottom: {
-        
+        textAlign: 'left',
+        maxWidth: 285
+    },
+    tempo: {
+        fontFamily: 'Poppins_500Medium',
+        fontSize: 12,
+        color: '#585858'
+    },
+    userBottom: {
+        fontFamily: 'Poppins_600SemiBold',
+        fontSize: 14,
+        color: '#000'
     }
 });
 
